af32c97dd0aed901a8f1454f7a0833d859789b88
Faza <fazarizqialfatih@gmail.com>
2020-11-25 17:51:40 +0700

    Pembaruan Data
    baca

--- kalkulator.php
+++ kalkulator.php
@@ -4,7 +4,10 @@ $angka1 = readline("Masukan Angka1: ");
 $angka2 = readline("Masukan Angka2: ");
 $option = readline("Masukan Kalkulasi: ");
 
-if (is_numeric($angka1) && is_numeric($angka2)) {
+$kalkulasi = ["+","-","*"];
+
+// in array adalah untuk mengecheck apakah value dalam array ada atau tidak, kiri = "input" kanan = "array"
+if (is_numeric($angka1) && is_numeric($angka2) && in_array($option,$kalkulasi)) {
 	if ($option == "+"){
 		$result = $angka1 + $angka2;
 
